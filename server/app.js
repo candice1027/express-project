69d0c538a563ac6c5555513703674eb17b67ca78
lvsuzhen <1205521940@qq.com>
2019-03-12 22:09:32 +0800

    设置跨域

--- server/app.js
+++ server/app.js
@@ -9,6 +9,13 @@ var logger = require('morgan');
 var apiRouter = require('./routes/api');
 
 var app = express();
+app.all('*',function(req,res,next){
+  res.header('Access-Control-Allow-Origin','*');
+  res.header('Access-Control-Allow-Headers','Content-Type');
+  res.header('Access-Control-Allow-Methods','*');
+  res.header('Content-Type','application/json;charset=uft-8');
+  next();
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
